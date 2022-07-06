9f06beee5e9fc9c793b2f4487cd8c2efeb4e65c2
KimJinsu66 <jinsu6688@gmail.com>
2022-07-06 23:46:02 +0900

    ver.1 구현, 리팩터링 필요

--- data_structure/binary_search.rb
+++ data_structure/binary_search.rb
@@ -1,3 +1,42 @@
-def binary_search(array, search_value)
+class BinarySearch
+  def initialize(array, search_value)
+    @array = array.sort
+    @search_value = search_value
+    @index = find_center_index
+  end
 
+  def search
+    p search_value
+  end
+
+  private
+
+  def search_value
+    index = @index
+    count = 0
+
+    while true do
+      count += 1
+
+      return index if @array[index] == @search_value
+
+      if @array[index] < @search_value
+        index += slice_half
+      else
+        index -= slice_half
+      end
+    end
+
+    count
+  end
+
+  def find_center_index
+    (@array.dup.length / 2) + 1
+  end
+
+  def slice_half
+    @index = @index / 2
+  end
 end
+
+BinarySearch.new(Array.new(1000) { |index| index }.shuffle, 243).search
