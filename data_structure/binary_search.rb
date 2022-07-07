ca17fbd68848dc47e0f254e91ea831c530e0ba1f
KimJinsu66 <jinsu6688@gmail.com>
2022-07-07 09:42:43 +0900

    shuffle, no shuffle 時間計算

--- data_structure/binary_search.rb
+++ data_structure/binary_search.rb
@@ -1,42 +1,96 @@
-class BinarySearch
+require 'benchmark'
+
+class BinarySearcher
   def initialize(array, search_value)
     @array = array.sort
     @search_value = search_value
     @index = find_center_index
+    @count = 0
   end
 
   def search
-    p search_value
+    p "Index: #{search_index}, Count: #{@count}"
   end
 
   private
 
-  def search_value
-    index = @index
-    count = 0
+  def search_index
+    basic_index = @index
 
     while true do
-      count += 1
+      return basic_index if @array[basic_index] == @search_value
 
-      return index if @array[index] == @search_value
+      @count += 1
 
-      if @array[index] < @search_value
-        index += slice_half
+      if @array[basic_index] < @search_value
+        @index += 1
+        basic_index += divided_two_index
       else
-        index -= slice_half
+        @index -= 1
+
+        if @index < 0
+          basic_index += divided_two_index
+          next
+        end
+
+        basic_index -= divided_two_index
       end
     end
-
-    count
   end
 
   def find_center_index
-    (@array.dup.length / 2) + 1
+    (@array.dup.length - 1) / 2
   end
 
-  def slice_half
-    @index = @index / 2
+  def divided_two_index
+    @index /= 2
   end
 end
 
-BinarySearch.new(Array.new(1000) { |index| index }.shuffle, 243).search
+Benchmark.bm do |x|
+  x.report('binary search') do
+    array = Array.new(1_000_000) { |index| index }.shuffle
+    search_value = 3500
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(3_023_000) { |index| index }.shuffle
+    search_value = 4200
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(5_467_323) { |index| index }.shuffle
+    search_value = 2_001
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(8767) { |index| index }.shuffle
+    search_value = 6700
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(130_232) { |index| index }.shuffle
+    search_value = 48_623
+    BinarySearcher.new(array, search_value).search
+  end
+end
+
+Benchmark.bm do |x|
+  x.report('binary search no shuffle') do
+    array = Array.new(1_000_000) { |index| index }
+    search_value = 3500
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(3_023_000) { |index| index }
+    search_value = 4200
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(5_467_323) { |index| index }
+    search_value = 2_001
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(8767) { |index| index }
+    search_value = 6700
+    BinarySearcher.new(array, search_value).search
+
+    array = Array.new(130_232) { |index| index }
+    search_value = 48_623
+    BinarySearcher.new(array, search_value).search
+  end
+end
--- data_structure/linear_search.rb
+++ data_structure/linear_search.rb
@@ -1,13 +1,70 @@
-def linear_search(array, search_value)
-  array.each_with_index do |element, index|
-    if element == search_value
-      return index
-    elsif element > search_value
-      break
-    end
+require 'benchmark'
+
+class LinearSearcher
+  def initialize(array, search_value)
+    @array = array
+    @search_value = search_value
   end
 
-  return nil
+  def search
+    @array.each_with_index do |element, index|
+      if element == @search_value
+        return index
+      elsif element > @search_value
+        break
+      end
+    end
+  end
 end
 
-p linear_search(Array.new(1000) { |index| index }, 100)
+array = Array.new(1_000_000) { |index| index }.shuffle
+search_value = 3500
+p LinearSearcher.new(array, search_value).search
+
+Benchmark.bm do |x|
+  x.report('linear search') do
+    array = Array.new(1_000_000) { |index| index }.shuffle
+    search_value = 3500
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(3_023_000) { |index| index }.shuffle
+    search_value = 4200
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(5_467_323) { |index| index }.shuffle
+    search_value = 5_467_323
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(8767) { |index| index }.shuffle
+    search_value = 6700
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(130_232) { |index| index }.shuffle
+    search_value = 48_623
+    LinearSearcher.new(array, search_value).search
+  end
+end
+
+Benchmark.bm do |x|
+  x.report('linear search no suffule') do
+    array = Array.new(1_000_000) { |index| index }
+    search_value = 3500
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(3_023_000) { |index| index }
+    search_value = 2_023_000
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(5_467_323) { |index| index }
+    search_value = 5_467_323
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(8767) { |index| index }
+    search_value = 6700
+    LinearSearcher.new(array, search_value).search
+
+    array = Array.new(130_232) { |index| index }
+    search_value = 70_000
+    LinearSearcher.new(array, search_value).search
+  end
+end
