d399fa0689c996bd5eb6e67bf24a81bb4b146b27
KimJinsu66 <jinsu6688@gmail.com>
2022-07-12 09:11:26 +0900

    약간의 코드 스타일 수정 & 테스트 케이스 추가

--- data_structure/bubble_sort.rb
+++ data_structure/bubble_sort.rb
@@ -6,20 +6,22 @@ class Bubble
 
   def sort
     while true do
-      break if @list_length == 0
+      break if @list_length.negative?
+
       0.upto(@list_length) do |i|
-        if @list[i] > @list[i + 1]
-          tmp = @list[i]
-          @list[i] = @list[i + 1]
-          @list[i + 1] = tmp
-        end
+        next unless @list[i] > @list[i + 1]
+
+        tmp = @list[i]
+        @list[i] = @list[i + 1]
+        @list[i + 1] = tmp
       end
 
       @list_length -= 1
     end
+
     p @list
   end
-
 end
 
 Bubble.new([99, 42, 20, 15, 67, 43, 120]).sort
+Bubble.new([100, 80, 60, 40, 20, 10, 1]).sort
