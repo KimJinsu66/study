911fed39470d61819b00c32e459081bb0323329f
KimJinsu66 <jinsu6688@gmail.com>
2022-07-18 10:57:37 +0900

    phone과 기본정책 구현을 ruby로 구현

--- ruby/oop/phone/phone.rb
+++ ruby/oop/phone/phone.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
-require './abstract_phone'
+require './rate_policy'
 
 # Description/Explanation of Phone class
-class Phone < AbstractPhone
-  def initialize(amount, seconds)
-    @amount = amount
-    @seconds = seconds
+class Phone
+  def initialize(start_time, end_time, calls)
+    @start_time = start_time
+    @end_time = end_time
+    @calls = []
   end
 
-  def calculate_call_fee; end
+  def calculate_fee
+    p "====================================="
+    p "calculate fee start "
+    p "====================================="
+
+    RatePolicy.new.calculate_fee(self)
+  end
+
+  def test_call
+    p "phone desu"
+  end
 end
+
+Phone.new("start_time", "end_time", [1, 3, 5]).calculate_fee
---
+++ ruby/oop/phone/rate_policy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Description/Explanation of RatePolicy class
+class RatePolicy
+  def calculate_fee(phone)
+    phone.test_call
+  end
+end
