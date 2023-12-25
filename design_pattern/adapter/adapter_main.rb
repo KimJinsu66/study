88014bb87dd79fadf49fa30d5a40265cdae444f3
KimJinsu66 <j_kim@ga-tech.co.jp>
2023-12-25 13:59:00 +0900

    adapter pattern

---
+++ design_pattern/adapter/adapter_main.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require './adapter'
+
+remote = SimpleRemoteControl.new
+light = Light.new
+
+light_on = LightOnCommand.new(light)
+light_off = LightOffCommand.new(light)
+
+remote.change_slot = light_on
+remote.button_was_pressed
+
+remote.change_slot = light_off
+remote.button_was_pressed
+
+garage_door = GarageDoor.new
+garage_door_open_command = GarageDoorOpenCommand.new(garage_door)
+
+remote.change_slot = garage_door_open_command
+remote.button_was_pressed
+
